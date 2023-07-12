6b72a8c9b66a57f6d131f1e26e0096cba13bdea9
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2023-07-12 12:46:57 +0530

    content corrected

--- Basic JavaScript/Q21 to Q40/32_find-the-length-of-a-string.js
+++ Basic JavaScript/Q21 to Q40/32_find-the-length-of-a-string.js
@@ -1,3 +1,7 @@
+//Find length of lastName
 
+lastName ="Hello, guys"
 // Only change code below this line
 lastNameLength = lastName.length;
+
+console.log(lastNameLength);
--- Basic JavaScript/Q21 to Q40/33_use-bracket-notation-to-find-the-first-character-in-a-string.js
+++ Basic JavaScript/Q21 to Q40/33_use-bracket-notation-to-find-the-first-character-in-a-string.js
@@ -1,3 +1,6 @@
+// assign first character to firstLetterOfLastName
+
+lastName="Hello World"
 
 // Only change code below this line
 firstLetterOfLastName = lastName[0]; // Change this line
